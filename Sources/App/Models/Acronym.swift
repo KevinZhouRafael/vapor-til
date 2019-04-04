24348d7e02d6343dab8d2a2a664465d38d6739d3
zhou kai <2008zkapie@163.com>
2019-04-04 12:29:45 +0800

    psql

---
+++ Sources/App/Models/Acronym.swift
@@ -0,0 +1,31 @@
+import Vapor
+import FluentPostgreSQL
+
+final class Acronym: Codable {
+    var id: Int?
+    var short: String
+    var long: String
+    init(short: String, long: String) {
+        self.short = short
+        self.long = long
+    }
+}
+
+//方式一
+//extension Acronym: Model {
+//    typealias Database = SQLiteDatabase
+//    typealias ID = Int
+//    public static var idKey: IDKey = \Acronym.id
+//}
+
+//方式二
+extension Acronym: PostgreSQLModel {}
+
+extension Acronym: Migration {}
+
+//json自动匹配
+//{
+//    "short": "OMG",
+//    "long": "Oh My God"
+//}
+extension Acronym: Content {}
